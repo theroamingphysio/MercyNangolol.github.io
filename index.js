f3d6efbd9fb6688ff7b4be1a9eec17737db299a7
Brivia Allan <140909644+briviamoon@users.noreply.github.com>
2024-12-20 19:52:03 +0300

    Setting up the first static release

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentloaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toogle(".active");
+    });
+});
